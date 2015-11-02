3310052e98700d7772d32efe5ccae2a04c40920a
Eric Reynolds <eric.remo.reynolds@gmail.com>
2015-11-02 14:44:45 +0100

    Added example

---
+++ examples/tests.py
@@ -0,0 +1,18 @@
+import unittest
+
+from nose_random import randomize
+
+class RandomTestCase(unittest.TestCase):
+    
+    def generate_scenario(self, rng):
+        return rng.random(), 10 * rng.random()
+        
+    @randomize(1000, generate_scenario)
+    def failing_test(self, scenario):
+        x, y = scenario
+        self.assertLess(x, y)
+
+    @randomize(1000, generate_scenario)
+    def passing_test(self, scenario):
+        x, y = scenario
+        self.assertLess(x, y + 1)
