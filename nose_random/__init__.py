3f648bbc6f2ed1f8ec8a25d7664804eada831561
Eric Reynolds <eric.remo.reynolds@gmail.com>
2015-11-02 11:28:31 +0100

    Command line argument works without nose too now

--- nose_random/__init__.py
+++ nose_random/__init__.py
@@ -3,35 +3,58 @@ __version__ = '0.0.1'
 from nose.plugins import Plugin
 import os
 
+from random import Random
 
-seed = None
+
+_missing = object()
+
+
+class NoseRandomConfig(object):
+    def __init__(self):
+        self.is_nose_plugin = False
+        self._scenario = _missing
+
+    @property
+    def scenario(self):
+        if self._scenario is _missing:
+            import sys
+            for arg in sys.argv:
+                if arg.startswith('--scenario='):
+                    self._scenario = arg[len('--scenario='):]
+        return self._scenario
+
+    @scenario.setter
+    def scenario(self, value):
+        self._scenario = value
+
+
+config = NoseRandomConfig()
 
 
 class NoseRandomPlugin(Plugin):
     def options(self, parser, env=os.environ):
-        parser.add_option('--scenario', type='int',
+        parser.add_option('--scenario', type='str',
                           dest='scenario',
                           help="Specify the scenario seed for debugging random tests.")
 
     def configure(self, options, conf):
-        global seed
-        seed = getattr(options, 'scenario', None)
+        config.scenario = getattr(options, 'scenario', None)
 
+
+def _generate_tag(n, rng):
+    return ''.join(rng.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789') for _ in range(n))
 
 
 def randomize(n, scenario_generator, seed=12038728732):
     def decorator(test):
         def randomized_test(self):
-            from random import Random
-            import nose_random
-            nseeds = n
-            if nose_random.seed is not None:
+            if config.scenario is not None:
                 nseeds = 1
-                seeds = [nose_random.seed]
+                seeds = [config.scenario]
             else:
                 rng_seed = Random(seed)
                 nseeds = n
-                seeds = (rng_seed.getrandbits(32) for i in range(n))
+                seeds = (_generate_tag(12, rng_seed) for i in range(n)) # (rng_seed.getrandbits(32) for i in range(n))
             for i, rseed in enumerate(seeds):
                 rng = Random(rseed)
                 scenario = scenario_generator(self, rng)
@@ -39,6 +62,6 @@ def randomize(n, scenario_generator, seed=12038728732):
                     test(self, scenario)
                 except Exception as e:
                     import sys
-                    raise type(e), type(e)('%s with scenario %i (%i of %i)' % (e.message, rseed, i+1, nseeds)), sys.exc_info()[2]
+                    raise type(e), type(e)('%s with scenario %s (%i of %i)' % (e.message, rseed, i+1, nseeds)), sys.exc_info()[2]
         return randomized_test
     return decorator
